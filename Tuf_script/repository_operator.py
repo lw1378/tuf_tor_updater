bba07080325ffe0a9596f4fe610ae1d138288bdf
Lai.wang <lw1378@nyu.edu>
2013-12-03 01:54:06 -0500

    Merge the update function into repository_operator.py

--- Tuf_script/repository_operator.py
+++ Tuf_script/repository_operator.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import shutil
+import tempfile
+from writemetainfo import create_metainfo_file
 from tuf.libtuf import *
 
 class Make_repository:
@@ -276,8 +278,60 @@ class Make_repository:
       shutil.rmtree(client_path)
     create_tuf_client_directory("path/to/repository/", "path/to/client/")
 
-def generate_repository(basic_directory, flag):
-  print '*** Hello ...'
+  def cp_update_files(self, src_path, dst_path):
+    # This is used when trying to update a directory.
+    # If a file has exist, then just replace it with a new version.
+    # When updating fails, then just return the original directory.
+    print '>>> Updating', dst_path, 'files ...'
+    src_fileList = os.listdir(src_path)
+    dst_fileList = os.listdir(dst_path)
+    tempdir = tempfile.mkdtemp() + "/"
+
+    try:
+      # Replace the old files and dirs in dst_path
+      for files in dst_fileList:
+        files_path = os.path.join(dst_path, files)
+        if files in src_fileList:
+          if os.path.isdir(files_path):
+            shutil.rmtree(files_path)
+            continue
+          os.remove(files_path)
+
+      self._copy_files(src_path, dst_path)
+      shutil.rmtree(tempdir)
+    except Exception, e:
+      # Failed to update the files and dirs
+      print 'Failed to update the files and dirs,', str(e)
+      dst_filesList = os.listdir(dst_path)
+      for files in dst_fileList:
+        files_path = os.path.join(dst_path, files)
+        if os.path.isdir(files_path):
+          shutil.rmtree(files_path)
+          continue
+        os.remove(files_path)
+      self._copy_files(tempdir, dst_path)
+      shutil.rmtree(tempdir)
+
+def copy_files_nd(src_name, src_path, dst_path):
+  # This is used when copying a specific file but not directory
+  if not os.path.exists(dst_path):
+    print 'Directory does not exist!'
+    sys.exit(0)
+
+  dst_fileList = os.listdir(dst_path)
+  for files in dst_fileList:
+    files_path = os.path.join(dst_path, files)
+    if files == src_name:
+      os.remove(files_path)
+
+  shutil.copy2(src_path, dst_path)
+
+def copy_files(src_path, dst_path):
+  mr = Make_repository()
+  mr.cp_update_files(src_path, dst_path)
+
+def generate_metadata(basic_directory, flag):
+  #print '*** Hello ...'
   if flag is True:
     mr = Make_repository()
     mr.init_state()
@@ -298,7 +352,7 @@ def generate_repository(basic_directory, flag):
     mr.make_client_dir()
   print '*** Process complete ...'
 
-def update_repository(basic_directory, flag):
+def update_metadata(basic_directory, flag):
   print '*** Hello ...'
   if flag is True:
     mr = Make_repository()
@@ -325,6 +379,217 @@ def generate_file_dir(basic_directory, flag):
       print 'Create directory django'
       os.makedirs(django_path)
 
+def generate_repository(basic_directory, flag):
+  print '*** Hello ...'
+  #generate_metadata(basic_directory, flag)
+
+  # If writemetainfo_dir is not found, then input the specific dir
+  base_path = os.path.dirname(__file__)
+  writemetainfo_dir_path = os.path.join(base_path, "writemetainfo_dir")
+  if not os.path.exists(writemetainfo_dir_path):
+    print 'Directory "writemetainfo_dir" does not exist.'
+    while True:
+      writemetainfo_dir_path = raw_input('Input writemetainfo directory:')
+      if os.path.exists(writemetainfo_dir_path):
+        print 'writemetainfo path: ', writemetainfo_dir_path
+        break
+      else:
+        print 'Directory is not found or the path is invalid!'
+
+  # Input key pairs for metainfo generation, default is lw1378.privatekey and lw1378.publickey
+  privatekey_path = os.path.join(base_path, "lw1378.privatekey")
+  if not os.path.exists(privatekey_path):
+    print 'privatekey for metainfo generation does not exist.'
+    while True:
+      privatekey_path = raw_input('Input privatekey path:')
+      if os.path.exists(privatekey_path):
+        print 'privatekey path: ', privatekey_path
+        break
+      else:
+        print 'Privatekey is not found.'
+
+  publickey_path = os.path.join(base_path, "lw1378.publickey")
+  if not os.path.exists(publickey_path):
+    print 'publickey for metainfo generation does not exist.'
+    while True:
+      publickey_path = raw_input('Input publickey path:')
+      if os.path.exists(publickey_path):
+        print 'publickey path: ', publickey_path
+        break
+      else:
+        print 'Publickey is not found.' 
+
+  # Input server path
+  while True:
+    server_path = raw_input('Input Server path:')
+    if os.path.exists(server_path):
+      print 'Server path: ', server_path
+      break
+    else:
+      print 'Server path is either invalid or server path does not exist!'
+
+  # Input client path
+  while True:
+    client_path = raw_input('Input client path:')
+    if os.path.exists(client_path):
+      print 'Clent path: ', client_path
+      break
+    else:
+      print 'client path is either invalid or client path does not exist!'
+  
+  # Now we have three paths and two parameters
+  # 1. writemetainfo_dir path
+  # 2. server_path
+  # 3. client_path
+  # 4. private key for metainfo generation
+  # 5. public key for metainfo generation
+
+  # First we generate a new temp targets directory
+  if os.path.exists(basic_directory):
+    shutil.rmtree(basic_directory)
+  generate_file_dir(basic_directory, flag)
+  # Then we might first want to generate a new metainfo for new files
+  create_metainfo_file(privatekey_path, publickey_path)
+  # Then we need to copy files with metainfo into our temp targets directory
+  writemetainfo_dir2basic_dir(writemetainfo_dir_path, basic_directory, flag)
+  # Now we can start generate a new repository
+  generate_metadata(basic_directory, flag)
+  # Update the client and server
+  rep_path = os.path.join(base_path, "path/to/repository")
+  copy_files(rep_path, server_path)
+  cli_path = os.path.join(base_path, "path/to/client")
+  copy_files(cli_path, client_path)
+
+def update_repository(basic_directory, flag):
+  print '*** Hello ...'
+  #generate_metadata(basic_directory, flag)
+
+  # If writemetainfo_dir is not found, then input the specific dir
+  base_path = os.path.dirname(__file__)
+  writemetainfo_dir_path = os.path.join(base_path, "writemetainfo_dir")
+  if not os.path.exists(writemetainfo_dir_path):
+    print 'Directory "writemetainfo_dir" does not exist.'
+    while True:
+      writemetainfo_dir_path = raw_input('Input writemetainfo directory:')
+      if os.path.exists(writemetainfo_dir_path):
+        print 'writemetainfo path: ', writemetainfo_dir_path
+        break
+      else:
+        print 'Directory is not found or the path is invalid!'
+
+  # Input key pairs for metainfo generation, default is lw1378.privatekey and lw1378.publickey
+  privatekey_path = os.path.join(base_path, "lw1378.privatekey")
+  if not os.path.exists(privatekey_path):
+    print 'privatekey for metainfo generation does not exist.'
+    while True:
+      privatekey_path = raw_input('Input privatekey path:')
+      if os.path.exists(privatekey_path):
+        print 'privatekey path: ', privatekey_path
+        break
+      else:
+        print 'Privatekey is not found.'
+
+  publickey_path = os.path.join(base_path, "lw1378.publickey")
+  if not os.path.exists(publickey_path):
+    print 'publickey for metainfo generation does not exist.'
+    while True:
+      publickey_path = raw_input('Input publickey path:')
+      if os.path.exists(publickey_path):
+        print 'publickey path: ', publickey_path
+        break
+      else:
+        print 'Publickey is not found.' 
+
+  # Input server path
+  while True:
+    server_path = raw_input('Input Server path:')
+    if os.path.exists(server_path):
+      print 'Server path: ', server_path
+      break
+    else:
+      print 'Server path is either invalid or server path does not exist!'
+
+  # First we generate a new temp targets directory
+  if os.path.exists(basic_directory):
+    shutil.rmtree(basic_directory)
+  generate_file_dir(basic_directory, flag)
+  # Then we might first want to generate a new metainfo for new files
+  create_metainfo_file(privatekey_path, publickey_path)
+
+  writemetainfo_dir2basic_dir(writemetainfo_dir_path, basic_directory, flag)
+  # Update metadata file
+  update_metadata(basic_directory, flag)
+  # Update the client and server
+  rep_path = os.path.join(base_path, "path/to/repository")
+  copy_files(rep_path, server_path)
+
+def writemetainfo_dir2basic_dir(writemetainfo_dir_path, basic_directory, flag):
+  # If we don't use delegation, then it would be easy for this step
+  if not flag:
+    # Copy all files in writemetainfo_dir to temp targets directory
+    copy_files(writemetainfo_dir_path, basic_directory)
+  else:
+    # If we use delegation now, the we should decide which directory we want to put into.
+    print 'With delegation, you have to decide files in targets and in django directories.'
+    print 'You could either choose files in writemetainfo dir or other files.'
+    print 'You could add files by input the item index.'
+    print '1. List all files in writemetainfo_dir.'
+    print '2. Add files in writemetainfo_dir to temp 1.targets or 2.django, syntax:'
+    print '   From, To  -->  file_index, dst_dir_index'
+    print '3. Add files in writemetainof_dir to temp targets dir, syntax: none'
+    print '4. Add files from other places to django delegation directory, syntax:'
+    print '   directly input file path'
+    print '5. exit'
+    wr_fileList = os.listdir(writemetainfo_dir_path)
+    while True:
+      main_op = input("Please enter the index:")
+      if not type(main_op) is int:
+        print 'Input should be a index integer!'
+        continue
+      elif main_op > 5 or main_op < 1:
+        print 'Input index must between 1 and 5!'
+        continue
+      if main_op == 1:
+        print 'Files in writemetainfo_dir'
+        for index in range(0, len(wr_fileList)):
+          print index, wr_fileList[index]
+        continue
+      if main_op == 2:
+        print 'Add files into 1. targets, 2. django'
+        try:
+          command_string = raw_input("Please input the index of files and dir that you want to input:")
+          file_index = command_string.split(",")[0]
+          dir_index = command_string.split(",")[-1]
+          file_index = file_index.replace(" ","")
+          dir_index = dir_index.replace(" ", "")
+          files_path = os.path.join(writemetainfo_dir_path, wr_fileList[int(file_index)])
+          if int(dir_index) == 1:
+            copy_files_nd(wr_fileList[int(file_index)], files_path, basic_directory)
+            print 'Successfully copy files to directory'
+          else:
+            dir_path = os.path.join(base_path, basic_directory)
+            dir_path = os.path.join(dir_path, "django")
+            copy_files_nd(wr_fileList[int(file_index)], files_path, dir_path)
+            print 'Successfully copy files to directory'
+        except Exception, e:
+          print 'Failed to achieve the goal, please try again!,', str(e)
+          continue
+      if main_op == 3:
+        print 'Copy all files in writemetainfo_dir to temp targets dir'
+        copy_files(writemetainfo_dir_path, basic_directory)
+        print 'Complete copying ...'
+        continue
+      if main_op == 4:
+        files_path = raw_input("Input file complete path:") 
+        if not os.path.exists(files_path):
+          print 'File does not exist!'
+          continue
+        dir_path = os.path.join(basic_directory, "django")
+        copy_files_nd(files_path.split("/")[-1], files_path, dir_path)
+        print 'Complete copying ...'
+      if main_op == 5:
+        break
+
 def help_info():
   print '*** Hello ...'
   print '--Help info--'
@@ -338,14 +603,14 @@ def help_info():
   print '$python repository_operator.py --generate_repository directory_name'
   print '2. Genrerate new TUF repository for update, syntax:'
   print '$python repository_operator.py --update_repository directory_name'
-  print '3. Generate temp copy directory, syntax:'
-  print '$python repository_operator.py --generate_file_dir directory_name'
-  print '4. Generate TUF repository without delegations, syntax:'
+  #print '3. Generate temp copy directory, syntax:'
+  #print '$python repository_operator.py --generate_file_dir directory_name'
+  print '3. Generate TUF repository without delegations, syntax:'
   print '$python repository_operator.py --generate_repository -nd directory_name'
-  print '5. Generate new TUF repository for update without delegations, syntax:'
+  print '4. Generate new TUF repository for update without delegations, syntax:'
   print '$python repository_operator.py --update_repository -nd directory_name'
-  print '6. Generate temp copy directory, syntax:'
-  print '$python repository_operator.py --generate_file_dir -nd directory_name'
+  #print '6. Generate temp copy directory, syntax:'
+  #print '$python repository_operator.py --generate_file_dir -nd directory_name'
   print '"""'
 
 if __name__ == '__main__':
@@ -357,8 +622,8 @@ if __name__ == '__main__':
       generate_repository(str(sys.argv[3]), False)
     elif sys.argv[1] == '--update_repository' and sys.argv[2] == '-nd':
       update_repository(str(sys.argv[3]), False)
-    elif sys.argv[1] == '--generate_file_dir' and sys.argv[2] == '-nd':
-      generate_file_dir(str(sys.argv[3]), False)
+    #elif sys.argv[1] == '--generate_file_dir' and sys.argv[2] == '-nd':
+      #generate_file_dir(str(sys.argv[3]), False)
     else:
       print 'Illegal args! Use "--help" to get more info.'
       sys.exit(0)
@@ -367,8 +632,8 @@ if __name__ == '__main__':
       generate_repository(str(sys.argv[2]), True)
     elif sys.argv[1] == '--update_repository':
       update_repository(str(sys.argv[2]), True)
-    elif sys.argv[1] == '--generate_file_dir':
-      generate_file_dir(str(sys.argv[2]), True)
+    #elif sys.argv[1] == '--generate_file_dir':
+      #generate_file_dir(str(sys.argv[2]), True)
     else:
       print 'Illegal args! Use "--help" to get more info.'
   elif len(sys.argv) == 2:
